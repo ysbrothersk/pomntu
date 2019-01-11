79e9f30cff89baea919d0992c216caa79c62f71c
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2019-01-11 20:40:49 +0900

    add: 時刻とボタンのUI追加

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,3 +1,24 @@
 <template>
-    <span></span>
+    <v-layout align-center justify-center row wrap>
+        <v-flex xs12>
+            <p class="display-3 mt-5 text-xs-center">{{time}}</p>
+        </v-flex>
+        <v-flex xs12 class="text-xs-center">
+            <div>
+                <v-btn class="red lighten-1 white--text">START</v-btn>
+                <v-btn class="ml-5 red lighten-1 white--text">DONE</v-btn>
+            </div>
+        </v-flex>
+    </v-layout>
 </template>
+
+<script>
+export default {
+    name: "Top",
+    data() {
+        return {
+            time: "25:00"
+        }
+    }
+}
+</script>
