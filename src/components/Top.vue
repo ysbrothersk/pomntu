6d88b342a975ca4bdd24bd591c35593028d6cf69
yasukotelin <yasukotelin@gmail.com>
2019-01-14 17:00:19 +0900

    add: カウントダウン機能の追加

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,24 +1,47 @@
 <template>
-    <v-layout align-center justify-center row wrap>
-        <v-flex xs12>
-            <p class="display-3 mt-5 text-xs-center">{{time}}</p>
-        </v-flex>
-        <v-flex xs12 class="text-xs-center">
-            <div>
-                <v-btn class="red lighten-1 white--text">START</v-btn>
-                <v-btn class="ml-5 red lighten-1 white--text">DONE</v-btn>
-            </div>
-        </v-flex>
-    </v-layout>
+  <v-layout align-center justify-center row wrap>
+    <v-flex xs12 class="text-xs-center mt-5">
+      <v-btn
+        class="text-xs-center display-2 circle white--text red lighten-1"
+        v-on:click="startCount()"
+      >{{time}}</v-btn>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-    name: "Top",
-    data() {
-        return {
-            time: "25:00"
-        }
+  name: "Top",
+  data() {
+    return {
+      millTime: 1500000 // 25min(mill seconds)
+    };
+  },
+  computed: {
+    time: function() {
+      const date = new Date(this.millTime);
+      const sec = date.getSeconds();
+      if (sec < 10) {
+        return date.getMinutes() + ":0" + sec;
+      } else {
+        return date.getMinutes() + ":" + date.getSeconds();
+      }
     }
-}
+  },
+  methods: {
+    startCount: function() {
+      setInterval(() => {
+        this.millTime = this.millTime - 1000;
+      }, 1000);
+    }
+  }
+};
 </script>
+
+<style scoped>
+.circle {
+  width: 250px;
+  height: 250px;
+  border-radius: 100%;
+}
+</style>
