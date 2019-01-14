9ca89406006e98a1aa5f720d6b30f72537796fc0
yasukotelin <yasukotelin@gmail.com>
2019-01-14 23:55:51 +0900

    add: タイマーの基本機能を追加

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -3,18 +3,30 @@
     <v-flex xs12 class="text-xs-center mt-5">
       <v-btn
         class="text-xs-center display-2 circle white--text red lighten-1"
-        v-on:click="startCount()"
+        v-on:click="onClickPomntuBtn()"
       >{{time}}</v-btn>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+let job;
+
+const State = {
+  standby: 1,
+  running: 2,
+  done: 3
+}
+
+const mill25min = 1500000
+const mill5min = 300000
+
 export default {
   name: "Top",
   data() {
     return {
-      millTime: 1500000 // 25min(mill seconds)
+      millTime: mill25min, // 25min(mill seconds)
+      state: State.standby
     };
   },
   computed: {
@@ -29,10 +41,40 @@ export default {
     }
   },
   methods: {
-    startCount: function() {
-      setInterval(() => {
-        this.millTime = this.millTime - 1000;
-      }, 1000);
+    onClickPomntuBtn: function() {
+      switch (this.state) {
+        case State.standby: {
+          this.state = State.running;
+
+          job = setInterval(() => {
+            this.millTime = this.millTime - 1000;
+          }, 1000);
+
+          break;
+        }
+        case State.running: {
+          this.state = State.done;
+          clearInterval(job);
+          this.millTime = mill5min
+
+          job = setInterval(() => {
+            if (this.millTime === 0) {
+              this.state = State.standby;
+              clearInterval(job)
+              this.millTime = mill25min;
+              return;
+            }
+            this.millTime = this.millTime - 1000;
+          }, 1000)
+          break;
+        }
+        case State.done: {
+          this.state = State.standby;
+          clearInterval(job)
+          this.millTime = mill25min;
+          break;
+        }
+      }
     }
   }
 };
