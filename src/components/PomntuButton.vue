ddb12dc74ab13bbc14ff1d76931231a59658249f
yasuhiro.kato <yasuhiro.kato@cct-inc.co.jp>
2019-01-24 10:08:04 +0900

    update: メインボタンのコンポーネント化（途中）

---
+++ src/components/PomntuButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-btn
+        class="text-xs-center display-2 circle white--text red lighten-1"
+        v-on:click="onClickPomntuBtn()"
+      >{{time}}</v-btn>
+</template>
+
+<script>
+export default {
+  name: "PomntuButton",
+  data() {
+    return {
+      time: "25:00"
+    };
+  },
+  methods: {
+    onClickPomntuBtn: function() {
+      alert("Push!")
+    }
+  }
+}
+</script>
+
+<style scoped>
+.circle {
+  width: 250px;
+  height: 250px;
+  border-radius: 100%;
+}
+</style>
