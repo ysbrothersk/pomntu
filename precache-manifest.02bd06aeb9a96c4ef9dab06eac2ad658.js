fb13ce3ec328b7301003f552f4fff83cd9daf216
yasukotelin <yasukotelin@gmail.com>
2019-03-27 13:09:37 +0900

    Updates

---
+++ precache-manifest.02bd06aeb9a96c4ef9dab06eac2ad658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e77253ffb3db40aa211e",
+    "url": "/pomntu/static/css/main.3e36c3d9.chunk.css"
+  },
+  {
+    "revision": "e77253ffb3db40aa211e",
+    "url": "/pomntu/static/js/main.90457271.chunk.js"
+  },
+  {
+    "revision": "ea6dc551dd014b3437a5",
+    "url": "/pomntu/static/js/runtime~main.538a187b.js"
+  },
+  {
+    "revision": "60da85bcf83af1d3bff7",
+    "url": "/pomntu/static/js/2.dcca7552.chunk.js"
+  },
+  {
+    "revision": "ab32eb27d1fa49577a8b21eb8da9a8b2",
+    "url": "/pomntu/index.html"
+  }
+];
