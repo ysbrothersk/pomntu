ac9206e884e485046d2d9f787b678fb965fee2ad
yasukotelin <yasukotelin@gmail.com>
2019-06-06 01:00:29 +0900

    Updates

---
+++ precache-manifest.b961ca6309727590a1cb7022c7df3c00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7c5ba1abbfc233aa9b58a729973e68",
+    "url": "/pomntu/index.html"
+  },
+  {
+    "revision": "1755b10fb00a43bb6e1a",
+    "url": "/pomntu/static/css/main.be601951.chunk.css"
+  },
+  {
+    "revision": "60da85bcf83af1d3bff7",
+    "url": "/pomntu/static/js/2.dcca7552.chunk.js"
+  },
+  {
+    "revision": "1755b10fb00a43bb6e1a",
+    "url": "/pomntu/static/js/main.6563cd94.chunk.js"
+  },
+  {
+    "revision": "ea6dc551dd014b3437a5",
+    "url": "/pomntu/static/js/runtime~main.538a187b.js"
+  }
+]);
